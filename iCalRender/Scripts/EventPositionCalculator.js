1d5fdfdc0f915cd4a12c656fb72bee7aa983e869
Taurit <git@taurit.pl>
2015-08-27 22:22:49 +0200

    Rendering single day is nearly ready, code needs a bit of refactoring

---
+++ iCalRender/Scripts/EventPositionCalculator.js
@@ -0,0 +1,154 @@
+﻿/// <reference path="ical.js" />
+/// <reference path="jquery-2.1.4.js" />
+
+/**
+ * Contains functions that help to calculate precise event position in the grid.
+ * 
+ * Rendering daily view of events requires solving a problem of displaying overlapping events.
+ * There seems to be no pure CSS solution to achieve this effect, and JavaScript is used to calculate events position in the grid.
+ * 
+ */
+
+
+// Namespace available globally
+ICalRender = {};
+
+ICalRender.EventPosition = function () {
+    /// <field type='Number' name='eventIndent'>Level of indent of given event in rendered calendar. Start from 0 (for events that should take full width of a container).</field>
+    /// <field type='Number' name='neighbourEvents'>Number of events to share width with (1 = this single event can take full width of a container)</field>
+
+    this.eventIndent = 0;
+    this.neighbourEvents = 1;
+
+};
+
+ICalRender.ExtendedEvent = function (basicEvent) {
+    /// <param name="basicEvent" type="ICAL.Event">Base event object</param>
+
+    /// <field name="basicEvent" type="ICAL.Event">Base event object</field>
+    /// <field name="startDate" type="Date">Start date from the event parsed to JS Date object</field>
+    /// <field name="endDate" type="Date">End date from the event parsed to JS Date object</field>
+
+    /// <field name="eventPosition" type="ICalRender.EventPosition">Calculated position of an event in a daytime grid</field>
+    /// <field name="processed" type="Boolean">Helper flag that states whether event has already been processed</field>
+
+    this.basicEvent = basicEvent;
+    this.startDate = new Date(basicEvent.startDate);
+    this.endDate = new Date(basicEvent.endDate);
+
+    this.processed = 0;
+    this.eventPosition = new ICalRender.EventPosition();
+}
+
+ICalRender.ExtendedEvent.prototype = {
+    overlaps: function (anotherEvent) {
+        /// <param name="anotherEvent" type="ICalRender.ExtendedEvent">Extended event object</param>
+
+        return (this.startDate < anotherEvent.endDate) && (this.endDate > anotherEvent.startDate);
+    },
+
+    overlapsWithAny: function (anotherEvents) {
+        /// <param name="anotherEvents" type="Array" elementtype="ICalRender.ExtendedEvent">Extended event array</param>
+
+        var thisEvent = this;
+        var result = false;
+        anotherEvents.forEach(function (anotherEvent) {
+            if ((thisEvent.startDate < anotherEvent.endDate) && (thisEvent.endDate > anotherEvent.startDate)) {
+                result = true;
+            }
+        });
+        return result;
+    },
+
+    findOverlapping: function (anotherEvents) {
+        /// <param name="anotherEvents" type="Array" elementtype="ICalRender.ExtendedEvent">Extended event array</param>
+        /// <returns type="Array" elementtype="ICalRender.ExtendedEvent">Events that overlap in time with a given one</returns>
+
+        var thisEvent = this;
+        var evnts = new Array();
+        anotherEvents.forEach(function (another) {
+            if (thisEvent.overlaps(another)) {
+                evnts.push(another);
+            }
+        });
+        return evnts;
+    }
+
+
+};
+
+ICalRender.EventPositionCalculator = function (allEventsInADay) {
+    /// <param name="allEventsInADay" type="Array" elementtype="ICAL.Event">List of all events that share the same container and may potentially overlap</param>
+    /// <field name="orderedEvents" type="Array" elementtype="ICalRender.ExtendedEvent">List of all events in a day ordered by their startDate</param>
+    var calc = this;
+
+    var ordEvents = allEventsInADay.sort(function (a, b) {
+        /// <param name="a" type="ICAL.Event"></param>
+        /// <param name="b" type="ICAL.Event"></param>
+        var dateA = new Date(a.startDate);
+        var dateB = new Date(b.startDate);
+        return dateA - dateB;
+    });
+
+    calc.orderedEvents = new Array();
+    ordEvents.forEach(function (evnt) {
+        var extendedEvent = new ICalRender.ExtendedEvent(evnt);
+        calc.orderedEvents.push(extendedEvent);
+    });
+
+
+};
+
+ICalRender.EventPositionCalculator.prototype = {
+
+    /**
+      * Processes given event list trying to find the best layout to display them and returns calculated metadata to rendering script.
+      * 
+      * @returns {ICalRender.ExtendedEvent} Event with specified indent
+      */
+    getEvents: function () {
+        var calc = this;
+        var maxNumSimultaneousEvents = 7; // arbitrary, max number of events that could be displayed near each other. too big will impact performance
+        for (var iterationNumber = 1; iterationNumber < maxNumSimultaneousEvents; iterationNumber++) {
+            var leftForDecision = calc.orderedEvents.filter(function (a) { return a.processed == 0; });
+            leftForDecision.forEach(function (unprocessedEvent) {
+                /// <param name="unprocessedEvent" type="ICalRender.ExtendedEvent">Event to find position for</param>
+
+                if (unprocessedEvent.overlapsWithAny(calc.orderedEvents.filter(function (a) { return a.processed == iterationNumber; }))) {
+                    // leave it for the next round
+                } else {
+                    unprocessedEvent.processed = iterationNumber;
+                    unprocessedEvent.eventPosition.eventIndent = iterationNumber - 1;
+                }
+
+            });
+        }
+
+        // set value for events stating with how many other events width of the container is shared
+        for (var iterationNumber = 2; iterationNumber < maxNumSimultaneousEvents; iterationNumber++) { // skip 1, neighbourEvents=1 by default
+            var levelEvents = calc.orderedEvents.filter(function (a) { return a.processed == iterationNumber; });
+            levelEvents.forEach(function (ev) {
+                /// <param name="ev" type="ICalRender.ExtendedEvent">Event with a given minimum number of neighbours</param>
+                var neighbours = ev.findOverlapping(calc.orderedEvents);
+                neighbours.forEach(function (overlappingEvent) {
+                    /// <param name="overlappingEvent" type="ICalRender.ExtendedEvent">Event that overlaps in time with a given one</param>
+
+                    overlappingEvent.eventPosition.neighbourEvents = iterationNumber;
+                });
+            });
+        }
+
+        // final round
+        for (var i = 0; i < calc.orderedEvents.length; i++) {
+            for (var j = i + 1; j < calc.orderedEvents.length; j++) {
+                if (calc.orderedEvents[i].overlaps(calc.orderedEvents[j])) {
+                    var max = Math.max(calc.orderedEvents[i].eventPosition.neighbourEvents, calc.orderedEvents[j].eventPosition.neighbourEvents);
+                    calc.orderedEvents[i].eventPosition.neighbourEvents = max;
+                    calc.orderedEvents[j].eventPosition.neighbourEvents = max;
+                }
+            }
+        }
+
+        return this.orderedEvents;
+    }
+};
--- iCalRender/Scripts/iCalRender.js
+++ iCalRender/Scripts/iCalRender.js
@@ -1,10 +1,15 @@
 ﻿/// <reference path="ical.js" />
 /// <reference path="jquery-2.1.4.js" />
+/// <reference path="EventPositionCalculator.js" />
 
 /**
  * iCalRender
  * 
  * Renders iCalendar-compatible file into HTML entirely on a client side (JavaScript).
+ * Use ical.js for parsing: https://github.com/mozilla-comm/ical.js/
+ * ical.js usage example: https://jsfiddle.net/kewisch/227efboL/
+ * 
+ * Visual Studio 2015 or newer with Web Essentials plugin is recommended to develop this solution.
  * 
  * Author: taurit
  * URL: http://icalrender.taurit.pl
@@ -13,11 +18,12 @@
  * 
  */
 
-// Namespace available globally
-ICalRender = {};
-
 (function () {
     "use strict";
+
+    //ICalRender.SimpleEvent = function (eventDate, eventEndDate, summary, description) {
+
+
 
     /**
      * Initialize renderer with a given calendar
@@ -26,8 +32,26 @@ ICalRender = {};
      */
     ICalRender.Renderer = function (iCalContent) {
         /// <field name="fileContent" type="String">Content of a user-provided iCalendar file</field>
+        /// <field name="parsedCalendarEvents" type="Array" elementType="ICAL.Event">Event list in a given calendar</field>
 
         this.fileContent = iCalContent;
+        this.parsedCalendarEvents = new Array();
+
+        var parsedCalendar = ICAL.parse(this.fileContent);
+
+        if (parsedCalendar) {
+            var renderer = this;
+
+            var vCalendar = new ICAL.Component(parsedCalendar);
+            var vEvents = vCalendar.getAllSubcomponents('vevent');
+
+            vEvents.forEach(function (vEvent) {
+                var evnt = new ICAL.Event(vEvent, {});
+                renderer.parsedCalendarEvents.push(evnt);
+            });
+        }
+
+
     };
 
     ICalRender.Renderer.prototype = {
@@ -40,10 +64,114 @@ ICalRender = {};
          * 
          * @returns {string} Rendered HTML code
          */
-
         RenderDay: function (year, month, day) {
-            return this.fileContent;
-        }
+            // find events that take place this day
+
+            var thisDayEvents = this.parsedCalendarEvents.filter(function (el) {
+                /// <param name="el" type="ICAL.Event">Single event</param>
+
+                var startDate = new Date(el.startDate);
+                var endDate = new Date(el.endDate);
+
+                var matchesFilter = startDate.getFullYear() === year
+                    && startDate.getMonth() + 1 === month // js starts month from 0
+                    && startDate.getDate() === day
+                    && !(startDate.getUTCHours() === 0
+                    && endDate.getDate() !== day);
+                return matchesFilter; // todo: insert date condition here
+            });
+
+            var thisDayAllDayEvents = this.parsedCalendarEvents.filter(function (el) {
+                /// <param name="el" type="ICAL.Event">Single event</param>
+
+                var startDate = new Date(el.startDate);
+                var endDate = new Date(el.endDate);
+
+                var matchesFilter = startDate.getFullYear() === year
+                    && startDate.getMonth() + 1 === month // js starts month from 0
+                    && startDate.getDate() === day
+                    && startDate.getUTCHours() === 0
+                    && endDate.getDate() !== day;
+                return matchesFilter; // todo: insert date condition here
+            });
+
+            // render
+            if (thisDayEvents.length + thisDayAllDayEvents.length > 0) {
+                var result = "";
+                result += '<div class="all-day-events">';
+                thisDayAllDayEvents.forEach(function (singleEvent) {
+                    /// <param name="singleEvent" type="ICAL.Event">Single event</param>
+                    var renderedEvent = "<div class='event'>" + singleEvent.summary + "</div>\n";
+                    result += renderedEvent;
+                });
+                result += '</div> <!-- / all-day-events -->';
+
+                result += '<div class="all-day-events-divider"></div>';
+
+                result += '<div class="scheduled-events">';
+                result += '<div class="time-grid">';
+                for (var hour = 0; hour < 24; hour++) {
+                    result += '<div class="time-grid-line"></div>';
+                }
+                result += '</div>';
+
+                var eventPosCalc = new ICalRender.EventPositionCalculator(thisDayEvents);
+                thisDayEvents = eventPosCalc.getEvents();
+
+                thisDayEvents.forEach(function (singleEvent) {
+                    /// <param name="singleEvent" type="ICalRender.ExtendedEvent">Single event with calculated position parameters</param>
+
+                    var startDate = new Date(singleEvent.basicEvent.startDate);
+                    var endDate = new Date(singleEvent.basicEvent.endDate);
+                    var startTotalMinutes = startDate.getHours() * 60 + startDate.getMinutes();
+                    var durationMin = (endDate - startDate) / (60 * 1000);
+
+                    var renderedEvent = "<div class='event scheduled-event' data-starttotalminutes='" + startTotalMinutes + "' data-duration='" + durationMin + "' data-numevents='" + singleEvent.eventPosition.neighbourEvents + "' data-eventord='" + singleEvent.eventPosition.eventIndent + "' title='" + singleEvent.basicEvent.summary + "'>" + singleEvent.basicEvent.summary + "</div>\n"; // todo: this is vulnerable to script injection, replace with use of templating engine or similar
+
+                    result += renderedEvent;
+                });
+
+                result += '</div> <!-- / scheduled-events -->';
+
+                return result;
+
+            } else {
+                return "<p data-i18n='ical.messages.noevents'>There are no events in this view.</p>";
+            }
+        },
+
+        /**
+         * Calculates new position for events (that have to be positioned absolutely) and apply them
+         * 
+         * @param {string} [containerSelector] jQuery selector targeting container where re-aligning should take place
+         */
+        RealignDays: function (containerSelector) {
+            $(containerSelector + " .day").each(function () {
+
+                var hourHeightPx = 40; // todo: deduce from css property, so there's no configuration needed after changing styles
+                var containerWidthPx = $(this).width() - 20; // -scrollbar width
+
+                $(this).find(".event.scheduled-event").each(function () {
+                    // rad and parse the data present in the event
+                    var startMinutes = $(this).data("starttotalminutes");
+                    var duration = $(this).data("duration");
+                    var numEvents = $(this).data("numevents");
+                    var eventOrd = $(this).data("eventord");
+
+
+                    var topPosition = (startMinutes / 60) * (hourHeightPx + 1); // +1 for divider line
+                    var eventWidthPx = containerWidthPx / numEvents;
+                    var leftPosition = (eventOrd * (eventWidthPx + 1));
+                    var eventHeightPx = hourHeightPx * duration / 60;
+
+                    $(this).css('position', 'absolute')
+                        .css('top', topPosition + 'px')
+                        .css('left', leftPosition + 'px')
+                        .css('width', eventWidthPx + 'px')
+                        .css('height', eventHeightPx + 'px');
+                });
+            });
+        },
     };
 })();
 
